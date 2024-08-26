2914283d4df3433f4078e90f3e25b76feac36bec
shehrozshafiq <shehrozshafiq@gmail.com>
2024-08-26 21:02:37 +0500

    Fixing accessability stuff via JS

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -31,4 +31,60 @@ add_action('wp_footer', 'customize_footer_info');
 
 // Dynamic Copyright Year End here
 
+function update_image_alt_tags_by_filename() {
+    ?>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+            var imagesToAltText = {
+                "Block2": "Family dependents",
+                "Picture4-241x300-1": "Mauritius Beachs",
+                "Picture5-300x178-1": "Mauritius villa",
+            };
+
+            var imgElements = document.querySelectorAll('img');
+
+            imgElements.forEach(function(imgElement) {
+                var imgSrc = imgElement.getAttribute('src');
+
+                if (imgSrc) {
+                    var imgName = imgSrc.split('/').pop().split('.')[0];
+
+
+                    if (imagesToAltText.hasOwnProperty(imgName)) {
+                        imgElement.alt = imagesToAltText[imgName];
+                        //console.log("Alt text set for: " + imgName);
+                    } else {
+                        console.log("No matching alt text found for: " + imgName);
+                    }
+                }
+            });
+        });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'update_image_alt_tags_by_filename');
+
+
+
+function add_inline_custom_script() {
+    ?>
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var anchorTag = document.querySelector('#block-30 a');
+        if (anchorTag) {
+            anchorTag.setAttribute('aria-label', 'Visit LinkedIn Profile');
+        }
+
+        var imgTag = document.querySelector('#block-30 img');
+        if (imgTag) {
+            imgTag.setAttribute('alt', 'LinkedIn Logo');
+        }
+    });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'add_inline_custom_script');
+
+
+
 ?>
